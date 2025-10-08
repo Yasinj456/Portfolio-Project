5e259b69bcbbe247a36420f78b961a72ba9f22af
Yasinj456 <yasinj456@gmail.com>
2025-10-08 17:18:30 -0400

    Created a private class to store exercise details

---
+++ src/WorkoutRoutineMVP.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class WorkoutRoutineMVP {
+    
+    
+    
+    private static class Exercise {
+        private String name;
+        private int sets;
+        private int reps;
+        private int weight;
+
+        Exercise(String name, int sets, int reps, int weight) {
+            this.name = name;
+            this.sets = sets;
+            this.reps = reps;
+            this.weight = weight;
+        }
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
